63703da8c85da5b1af594325fd0f0415f9521564
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-08-08 22:37:25 +0100

    task 8 update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def uppercase(str):
     for char in str:
         ascii_num = ord(char)
-        if 97 <= ascii_num <=122:
+        if 97 <= ascii_num <= 122:
             uppercase = chr(ascii_num - 32)
         else:
             uppercase = char
