00604a8deae9842fa27be2ed8e66f45f3ec3c699
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-08-08 22:30:27 +0100

    update task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for i in range(0,99):
-    if i != 89 and ((i//10) < (i % 10)):
-        print(f'{i // 10}{i % 10},', end =" ")
-    elif (i//10) >= (i % 10):
+for i in range(0, 99):
+    if i != 89 and ((i // 10) < (i % 10)):
+        print("{}{},".format((i // 10), (i % 10)), end =" ")
+    elif (i // 10) >= (i % 10):
         continue
     else:
-        print(f'{i // 10}{i % 10}')
+        print("{}{}".format((i // 10), (i % 10)))
