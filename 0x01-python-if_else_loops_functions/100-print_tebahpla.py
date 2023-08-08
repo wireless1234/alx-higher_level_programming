f092df323a1bb1c627690ced9d046680dd055e19
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-08-08 23:09:22 +0100

    100 updates

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,4 +3,4 @@ for i in range(122, 96, -1):
     if i % 2 == 0:
         print("{}".format(chr(i)),end="")
     else:
-        print("{}".format(chr(i - 32),end="")
+        print("{}".format(chr(i - 32)),end="")
