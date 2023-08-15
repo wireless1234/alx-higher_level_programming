db5a3534dcc42488a56743f186f9c78a71fa9d17
Isa Sulaiman <isasulaimanisa@gmail.com>
2023-08-15 02:55:19 +0100

    added condition to loop

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
     my_list[idx] = element
     return my_list
