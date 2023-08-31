a779c9f9957efe44e48f3df726d337702ed5e84f
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-08-30 18:48:14 -0700

    update 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
-	try:
-	    print("{}".format(my_list[i]), end='')
-	    count += 1
-	except IndexError:
-	    continue
+        try:
+            print("{}".format(my_list[i]), end='')
+            count += 1
+        except IndexError:
+            continue
     print()
     return count
