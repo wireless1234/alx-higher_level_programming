4f80a95299ddf32254b33df9d10d4bb9cedb8ec5
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-09-16 14:15:17 +0100

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,10 +2,12 @@
 """
 Module doctring
 """
+
+
 def read_file(filename=""):
-	"""
-	reads file with filename as name
-	"""
-	with open(filename, encoding='UTF-8') as f:
-		for line in f:
-			print(line, end='')
+    """
+    reads file with filename as name
+    """
+    with open(filename, encoding='UTF-8') as f:
+        for line in f:
+            print(line, end='')
