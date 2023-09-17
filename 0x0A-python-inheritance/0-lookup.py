3d116cf857702d98e05d15fb987b8228c788dc73
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-09-17 14:13:14 +0100

    task 0 update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Module contains lookup function
+"""
+Module contains lookup function
 """
 
 
 def lookup(obj):
-    """returns the list of available attributes and methods
+    """
+    Returns the list of available attributes and methods
     Args:
         obj: An object
     Returns: attributes of object
