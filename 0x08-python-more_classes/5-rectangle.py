258e82718123d48eea4f77fdc4385e8b26d0be14
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-09-28 16:13:56 +0100

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -56,5 +56,6 @@ class Rectangle:
 
     def __repr__(self):
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-    def __del__(self):
+    @staticmethod
+    def __del__():
         print("Bye rectangle...")
