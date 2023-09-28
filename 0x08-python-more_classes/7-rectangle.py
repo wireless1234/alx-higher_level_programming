d3ebbcb1ce461fba00d849c3139b5010b5212a49
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-09-28 16:48:15 +0100

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -53,7 +53,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return mystr
         for i in range(self.__height):
-            mystr += "{}".format(self.print_symbol * self.__width)
+            mystr += "{}".format(str(self.print_symbol) * self.__width)
             if i is not self.__height - 1:
                 mystr += '\n'
         return mystr
