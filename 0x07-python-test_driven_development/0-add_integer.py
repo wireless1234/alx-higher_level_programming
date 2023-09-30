f14156b4f196dcf75e7eeaa109b5c2e643662d73
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-09-30 12:30:21 +0100

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""This module contains the add integer function
+"""
+
+
 def add_integer(a, b=98):
+    """Adds two integers
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
