a6196a9a13f4c121d4545532150fe43f8cda546c
ISA SULAIMAN ISA <isasulaimanisa@gmail.com>
2023-10-02 22:57:12 +0100

    updates

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-""" Module defines rectangle class
+"""
+Module defines rectangle class
 rectangle class inherits base geometry
 """
 
 
 class Rectangle(BaseGeometry):
     """Rectangle class definition
+    Rectangle inherits base geometry
     """
 
     def __init__(self, width, height):
