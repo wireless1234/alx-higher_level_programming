7d673cc00805c2ad015c33e93aa2729a82ea8428
isa sulaiman isa <isasulaimanisa@gmail.com>
2024-07-02 14:59:16 +0100

    fixed semistandard for task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -6,7 +6,7 @@
  */
 const { argv } = require('node:process');
 
-if ( argv[2] && !isNaN(argv[2]) ) {
+if (argv[2] && !isNaN(argv[2])) {
   console.log(`My number: ${parseInt(argv[2])}`);
 } else {
   console.log('Not a number');
